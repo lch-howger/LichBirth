cadfa4fe574ca37e3111220a66e06b5c51629adb
lch-howger <360000521@qq.com>
2020-06-01 00:00:26 +0100

    Create BirthEntity.java

---
+++ app/src/main/java/com/lich/lichbirth/entity/BirthEntity.java
@@ -0,0 +1,27 @@
+package com.lich.lichbirth.entity;
+
+/**
+ * Created by lichhowger on 2020/5/31.
+ */
+public class BirthEntity {
+    private String Id;
+    private String name;
+    private String time;
+    private String type;
+
+    public String getId() {
+        return Id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public String getType() {
+        return type;
+    }
+}
